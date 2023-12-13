88834313540850e97e53e3a1e61875e6a8fae8e6
jongsng <dlgksquf054@naver.com>
2023-12-13 09:36:55 +0900

    Initial commit

---
+++ src/test/java/com/lec/spring/controller/HomeControllerTest.java
@@ -0,0 +1,30 @@
+package com.lec.spring.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HomeControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void hello() {
+       ResponseEntity<String> response = restTemplate.getForEntity("/aws/v1", String.class);
+       assertEquals("<h1>AWS 배포 v1</h1>", response.getBody());
+    }
+
+}
+
+
+
+
+
+
+
